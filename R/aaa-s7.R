3df07c4182d46dee34170afb3928819d9e684005
Gus Lipkin <gus.lipkin@gmail.com>
2024-06-12 12:47:25 -0400

    renamed file and backend improvements

--- R/aaa-s7.R
+++ R/aaa-s7.R
@@ -4,13 +4,19 @@ faketable <- S7::new_class(
   properties = list( # .prop values are read-only
     'x' = S7::class_data.frame,
     '.raw_data' = S7::class_data.frame,
-    '.updated_data' = S7::new_property(
+    '.inserted' = S7::new_property(
+      class = S7::class_data.frame,
+      getter = \(self) {
+        dplyr::anti_join(self@x, self@.raw_data, by = self@.rowId)
+      }
+    ),
+    '.updated' = S7::new_property(
       class = S7::class_data.frame,
       getter = \(self) {
         dplyr::anti_join(self@x, self@.raw_data, by = colnames(self@.raw_data))
       }
     ),
-    '.removed_data' = S7::new_property( # just the rows that have been removed
+    '.deleted' = S7::new_property( # just the rows that have been removed
       class = S7::class_data.frame,
       getter = \(self) {
         dplyr::anti_join(self@.raw_data, self@x, by = self@.rowId)
@@ -33,16 +39,21 @@ faketable <- S7::new_class(
     if (is.null(x[[rowId]])) {
       x <-
         x |>
+        dplyr::mutate('.rowId' = dplyr::row_number()) |>
         dplyr::mutate(
-          {{rowId}} := purrr::map_chr(dplyr::row_number(), digest::digest),
-          .before = 0
-        )
+          {{rowId}} := purrr::map_chr(.data, digest::digest),
+          .before = 0,
+          .by = '.rowId'
+        ) |>
+        dplyr::select(-'.rowId')
     }
 
     S7::new_object(
       S7::S7_object(),
       'x' = x,
       '.raw_data' = x,
+      # '.updated' = head(x, 0),
+      # '.deleted' = head(x, 0),
       '.table_def' = table_def,
       '.rowId' = rowId,
       '.show_delete' = show_delete
