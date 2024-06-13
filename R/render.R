01c2de7bdda40b9edd2b97c0830e34e9a949ad7e
Gus Lipkin <gus.lipkin@gmail.com>
2024-06-12 22:37:00 -0400

    render methods don't need to be generics

--- R/render.R
+++ R/render.R
@@ -1,3 +1,20 @@
+.render_header <- function(f_tab) {
+  shiny::removeUI(
+    selector = glue::glue("#table-header .table-header"),
+    multiple = TRUE
+  )
+
+  shiny::insertUI(
+    selector = '#table-header',
+    where = 'afterBegin',
+    ui = shiny::tags$div(
+      .create_table_header(f_tab),
+      shiny::tags$hr(style = 'margin-top: 0;'),
+      class = 'table-header'
+    )
+  )
+}
+
 .create_table_header <- function(f_tab) {
   cols <-
     f_tab@.table_def |>
@@ -23,22 +40,8 @@
   shiny::fluidRow(cols, class = 'table-header-row')
 }
 
-.render_header <- S7::new_generic('.render_header', 'f_tab')
-S7::method(.render_header, faketable) <- function(f_tab) {
-  shiny::removeUI(
-    selector = glue::glue("#table-header .table-header"),
-    multiple = TRUE
-  )
-
-  shiny::insertUI(
-    selector = '#table-header',
-    where = 'afterBegin',
-    ui = shiny::tags$div(
-      .create_table_header(f_tab),
-      shiny::tags$hr(style = 'margin-top: 0;'),
-      class = 'table-header'
-    )
-  )
+.render_table <- function(f_tab, ns) {
+  .create_table_body(f_tab, ns)
 }
 
 .create_table_body <- function(f_tab, ns) {
@@ -76,9 +79,4 @@ S7::method(.render_header, faketable) <- function(f_tab) {
       return(cols)
     }) |>
     purrr::map(shiny::fluidRow, class = 'table-row')
-}
-
-.render_table <- S7::new_generic('.render_table', 'f_tab')
-S7::method(.render_table, faketable) <- function(f_tab, ns) {
-  .create_table_body(f_tab, ns)
 }
