26de1d529dea328404e1632b81d7e33d145314a5
Gus Lipkin <gus.lipkin@gmail.com>
2024-06-14 16:22:55 -0400

    making it work with shinyapps

--- app.R
+++ app.R
@@ -1,96 +1,251 @@
-ui <- shiny::fluidPage(
-  faketablesUI(),
-  shiny::tags$h3('Add Row'),
-  shiny::fluidRow(
-    shiny::column(
-      width = 2,
-      shiny::actionButton('add_row', 'Add Row')
-    ),
-    shiny::column(
-      width = 3,
-      shiny::textInput('mpg', 'MPG', value = 50)
-    ),
-    shiny::column(
-      width = 3,
-      shiny::selectInput('cyl', 'CYL', c(4, 6, 8))
-    ),
-    shiny::column(
-      width = 3,
-      shiny::sliderInput('qsec', 'QSEC', min = 10, max = 25, value = c(10, 25))
-    )
-  ),
-  shiny::tags$h3('Table'),
-  shiny::tableOutput('table'),
-  shiny::tags$h3('Inserted'),
-  shiny::tableOutput('inserted'),
-  shiny::tags$h3('Updated'),
-  shiny::tableOutput('updated'),
-  shiny::tags$h3('Deleted'),
-  shiny::tableOutput('deleted'),
-  # shiny::tags$h3('Misc.'),
-  # shiny::uiOutput('misc')
-)
-
-server <- function(input, output, session) {
-  f_tab <- faketablesServer(faketable = f_tab)
-  output$table <- shiny::renderTable(.list_col_to_chr(f_tab()@x))
-  output$inserted <- shiny::renderTable(.list_col_to_chr(f_tab()@inserted))
-  output$updated <- shiny::renderTable(.list_col_to_chr(f_tab()@updated))
-  output$deleted <- shiny::renderTable(.list_col_to_chr(f_tab()@deleted))
-
-  shiny::observe({
-    ins <- tibble::tibble(
-      'mpg' = as.numeric(input$mpg),
-      'cyl' = as.numeric(input$cyl),
-      'qsec' = list(as.integer(input$qsec))
-    ) |>
-      .create_rowid()
-    f_tab <- faketablesServer(faketable = insert(f_tab(), ins))
-  }) |>
-    shiny::bindEvent(input$add_row, ignoreInit = TRUE)
-}
-
+## ---- col_def --------
 c_def <- list(
-  col_def(
-    name = 'mpg',
-    input = input_call(
+  faketables::col_def(
+    name = 'Name',
+    input = faketables::input_call(
       fun = \(...) { shinyjs::disabled(shiny::textInput(...)) },
-      args = list(label = NULL, placeholder = 'mpg')
+      args = list(label = NULL)
     ),
-    cast = as.numeric,
-    width = 3,
-    display_name = 'MPG'
+    cast = as.character,
+    width = 2
   ),
-  col_def(
-    name = 'cyl',
-    input = input_call(
+  faketables::col_def(
+    name = 'Address',
+    input = faketables::input_call(
+      fun = shiny::textInput,
+      args = list(label = NULL)
+    ),
+    cast = as.character,
+    width = 2
+  ),
+  faketables::col_def(
+    name = 'City',
+    input = faketables::input_call(
       fun = shiny::selectInput,
-      args = list(label = NULL, choices = c(4, 6, 8))
+      args = list(
+        label = NULL,
+        choices = c('Bronx', 'Brooklyn', 'New York', 'Queens', 'Staten Island')
+      )
+    ),
+    cast = as.character,
+    width = 2
+  ),
+  faketables::col_def(
+    name = 'Zip',
+    input = faketables::input_call(
+      fun = shiny::numericInput,
+      args = list(label = NULL, min = 10001, max = 11697, step = 1)
     ),
     cast = as.integer,
-    width = 3,
-    display_name = 'CYL'
+    width = 1
   ),
-  col_def(
-    name = 'qsec',
-    input = input_call(
-      fun = shiny::sliderInput,
-      args = list(label = 'Quarter Mile', min = 10, max = 25)
+  faketables::col_def(
+    name = 'FavoritePizza',
+    input = faketables::input_call(
+      fun = shiny::textInput,
+      args = list(label = NULL)
     ),
-    cast = \(x) purrr::map(x, as.numeric),
-    width = 3,
-    display_name = 'QSEC'
+    cast = as.character,
+    width = 2,
+    display_name = 'Favorite Pizza'
+  ),
+  faketables::col_def(
+    name = 'Rating',
+    input = faketables::input_call(
+      fun = shiny::sliderInput,
+      args = list(label = NULL, min = 11, max = 20)
+    ),
+    cast = as.integer,
+    width = 2
   )
 )
 
-t_def <- table_def(c_def)
+## ---- table_def --------
+t_def <- faketables::table_def(c_def)
 
-f_tab <-
-  mtcars |>
+## ---- faketables --------
+pz <-
+  jsonlite::read_json(
+    path = 'https://www.jaredlander.com/data/FavoritePizzaPlaces.json',
+    simplifyVector = TRUE
+  ) |>
   tibble::as_tibble() |>
-  head(3) |>
-  dplyr::mutate('qsec' = purrr::map(.data$qsec, \(x) round(c(x - 1, x + 1)))) |>
-  dplyr::select('mpg', 'cyl', 'qsec') |>
-  faketable(t_def, show_delete = list())
+  tidyr::unnest(cols = c('Details', 'Coordinates')) |>
+  dplyr::mutate(
+    'Zip' = as.integer(.data$Zip),
+    'FavoritePizza' = 'Cheese',
+    'Rating' = 11L
+  ) |>
+  faketables::faketable(table_def = t_def, show_delete = list(width = 1))
 
+## ---- ui --------
+ui <- bslib::page_navbar(
+  title = 'Favorite Pizza Places',
+  bslib::nav_panel(
+    title = 'NYC',
+    bslib::layout_columns(
+      col_widths = c(4, 8),
+      min_height = '500px',
+      bslib::card(
+        bslib::card_header('Add Favorite'),
+        bslib::card_body(
+          shiny::fluidRow(
+            shiny::column(
+              width = 12,
+              shiny::fluidRow(
+                shiny::column(
+                  width = 6,
+                  shiny::textInput('name', label = 'Name')
+                ),
+                shiny::column(
+                  width = 6,
+                  shiny::textInput('address', label = 'Address')
+                )
+              ),
+              shiny::fluidRow(
+                shiny::column(
+                  width = 6,
+                  shiny::selectInput(
+                    'city',
+                    label = 'City',
+                    choices = c('Bronx', 'Brooklyn', 'New York', 'Queens', 'Staten Island'),
+                    selected = 'New York')
+                ),
+                shiny::column(
+                  width = 6,
+                  shiny::numericInput(
+                    'zip',
+                    label = 'Zip',
+                    value = 10001,
+                    min = 10001,
+                    max = 11697,
+                    step = 1)
+                  )
+              ),
+              shiny::fluidRow(
+                shiny::column(
+                  width = 6,
+                  shiny::textInput('latitude', label = 'Latitude')
+                ),
+                shiny::column(
+                  width = 6,
+                  shiny::textInput('longitude', label = 'Longitude')
+                )
+              ),
+              shiny::fluidRow(
+                shiny::column(
+                  width = 6,
+                  shiny::textInput('favoritePizza', label = 'Favorite Pizza')
+                ),
+                shiny::column(
+                  width = 6,
+                  shiny::sliderInput(
+                    'rating',
+                    label = 'Pizza Rating',
+                    min = 11,
+                    max = 20,
+                    value = 11)
+                )
+              )
+            )
+          ),
+          shiny::fluidRow(
+            shiny::actionButton('add', label = 'Add Data')
+          )
+        )
+      ),
+      bslib::card(
+        full_screen = TRUE,
+        bslib::card_header('Map'),
+        bslib::card_body(
+          leaflet::leafletOutput(outputId = 'map'),
+        )
+      )
+    ),
+    bslib::card(
+      full_screen = TRUE,
+      bslib::card_header('Table'),
+      bslib::card_body(
+        faketables::faketablesUI()
+      )
+    )
+  )
+)
+
+## ---- server --------
+server <- function(input, output, session) {
+  pz <- faketables::faketablesServer(faketable = pz)
+  output$map <-
+    leaflet::leaflet() |>
+    leaflet::addTiles() |>
+    leaflet::setView(-74.0060, 40.7128, 11) |>
+    leaflet::renderLeaflet()
+
+  map_data <- shiny::reactive({
+    pz()@x |>
+      dplyr::mutate(
+        'label' = shiny::HTML(glue::glue('{.data$Name}<br>{.data$Address}, {.data$City}, NY, {.data$Zip}')),
+        .by = '.rowId'
+      ) |>
+      dplyr::mutate(
+        'fill_color' = dplyr::case_when(
+          stringr::str_detect(tolower(.data$FavoritePizza), 'cheese') ~ '#FFCA45'
+        )
+      )
+  })
+
+  shiny::observe({
+    ins <- tibble::tibble(
+      'Name' = input$name,
+      'Address' = input$address,
+      'City' = input$city,
+      'State' = 'NY',
+      'Zip' = as.integer(input$zip),
+      'latitude' = as.numeric(input$latitude),
+      'longitude' = as.numeric(input$longitude),
+      'FavoritePizza' = input$favoritePizza,
+      'Rating' = as.integer(input$rating)
+    ) |>
+      suppressWarnings()
+    if (any(is.na(unlist(ins)) | unlist(ins) == '')) {
+      shiny::showNotification(
+        'Something went wrong. Please validate your input data and try again.',
+        type = 'error'
+      )
+    } else {
+      pz <- faketables::faketablesServer(faketable = faketables::insert(pz(), ins))
+    }
+  }) |>
+    shiny::bindEvent(input$add)
+
+  shiny::observe({
+    map_data <-
+      pz()@x |>
+      dplyr::mutate(
+        'label' = shiny::HTML(glue::glue('{.data$Name}<br>{.data$Address}, {.data$City}, NY, {.data$Zip}')),
+        .by = '.rowId'
+      ) |>
+      dplyr::mutate(
+        'fill_color' = dplyr::case_when(
+          stringr::str_detect(tolower(.data$FavoritePizza), 'cheese') ~ '#FFCA45'
+        )
+      )
+    leaflet::leafletProxy('map', data = map_data) |>
+      leaflet::clearMarkers() |>
+      leaflet::addCircleMarkers(
+        lng = ~longitude,
+        lat = ~latitude,
+        radius = 15,
+        color = '#C9AA6E',
+        weight = 4,
+        opacity = 1,
+        fillColor = ~fill_color,
+        fillOpacity = 1,
+        label = ~label
+      )
+  }) |>
+    shiny::bindEvent(pz(), input$add)
+}
+
+## ---- run --------
 shiny::shinyApp(ui, server)
